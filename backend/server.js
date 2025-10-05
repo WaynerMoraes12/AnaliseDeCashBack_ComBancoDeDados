6bacf6f485fff2e1ccb385e9c8e6688b191496fa
WaynerMoraes12 <waynerbusiness@outlook.com>
2025-10-05 02:47:22 -0300

    Trablaho finalizado

--- backend/server.js
+++ backend/server.js
@@ -6,43 +6,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configuração do SQL Server
 const config = {
-    user: 'meliuz_user', // ou deixe vazio se usar Windows Authentication
-    password: 'Meliuz@123', // ou deixe vazio
-    server: 'localhost', // ou o nome que funcionou no SSMS
+    server: 'WAYNERMORAES',
     database: 'MeliuzDB',
+    user: 'sa', 
+    password: 'NovaSenhaSuperForte123',
+    
     options: {
-        encrypt: false, // Desabilita SSL
-        trustServerCertificate: true
+        trustServerCertificate: true,
+        encrypt: true 
     }
 };
+console.log(">>>>>> EXECUTANDO A VERSÃO MAIS RECENTE DO CÓDIGO <<<<<<"); 
+sql.connect(config).then(pool => {
+    if (pool.connected) {
+        console.log('Conexão com o SQL Server bem-sucedida!');
+    }
 
-// Rota de teste
-app.get('/', (req, res) => {
-    res.send('Servidor backend rodando!');
-});
+    app.get('/', (req, res) => {
+        res.send('Servidor backend rodando!');
+    });
 
-// Rota para buscar dados do SQL Server
-app.get('/dados', async (req, res) => {
+ app.get('/dados', async (req, res) => {
     try {
-        await sql.connect(config);
+        
         const result = await sql.query`
             SELECT 
-                MONTH(Data) as month,
+                Mes as month,
                 Parceiro as partner,
-                SUM(ValorCompra) as revenue,
-                COUNT(*) as transactions,
-                SUM(CASE WHEN NovoUsuario = 1 THEN 1 ELSE 0 END) as newUsers,
-                SUM(Comissao) as commission
-            FROM Transacoes
-            GROUP BY MONTH(Data), Parceiro
+                Receita as revenue,
+                Transacoes as transactions,
+                NovosUsuarios as newUsers,
+                Comissao as commission
+            FROM 
+                performance
         `;
         res.json(result.recordset);
+
     } catch (err) {
         console.error('Erro ao buscar dados:', err);
-        res.status(500).json({ error: 'Erro ao buscar dados' });
-    }
+        res.status(500).json({ error: 'Erro ao buscar dados do servidor' });
+    }
 });
+    app.listen(5000, () => console.log('Servidor rodando em http://localhost:5000'));
 
-app.listen(5000, () => console.log('Servidor rodando em http://localhost:5000'));
+}).catch(err => {
+    console.error('Erro ao conectar ao SQL Server:', err);
+});
